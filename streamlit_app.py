df58d81b8dd67385cccecd85700fac3742230a36
StepanNOQA <108218323+StepanNOQA@users.noreply.github.com>
2024-01-29 18:37:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 streamlit.title('Snowflake Data Application Builders Workshop')
 
-streamlit.header('Build Your Own Fruit Smoothie')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
